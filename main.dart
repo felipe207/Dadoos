9b45b5b9be39e52cd2d077798c942ac68347c70f
Felipe Pinheiro <68909633+felipe207@users.noreply.github.com>
2021-11-25 20:14:02 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatefulWidget {
+//  @override
+//  _DadoosState createState() => _DadoosState();
+//}
+
+//class _DadooState extends State<Dadoos> {
+  @override
+  State<Dadoos> createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int numeroDadoEsquerda = 1;
+  int numeroDadoDireita = 1;
+
+  void alterarFaceDosDados() {
+    setState(() {
+      numeroDadoEsquerda = Random().nextInt(6) + 1;
+      numeroDadoDireita = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                alterarFaceDosDados();
+              },
+              child: Image.asset('imagens/dado$numeroDadoEsquerda.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                alterarFaceDosDados();
+              },
+              child: Image.asset('imagens/dado$numeroDadoDireita.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
